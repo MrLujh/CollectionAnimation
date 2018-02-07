2b9e2f2878192245993541eeb4db3eb0e47aeef0
daniulaolu <287929070@qq.com>
2018-02-07 18:04:14 +0800

    Initial Commit

---
+++ CollectionAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionAnimation
+//
+//  Created by lujh on 2018/2/7.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
